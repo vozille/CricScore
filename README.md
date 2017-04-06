f9db1ba60c787c249408103f96ad901473733c61
Anwesh Mohanty <anwesh063@gmail.com>
2017-04-06 09:01:08 +0530

    v 1.0

---
+++ README.md
@@ -0,0 +1,20 @@
+# CricScore
+
+Fetches live scores from cricbuzz.
+Shows scores as a notification periodically.
+
+#Usage
+
+* Clone the repository
+* Install the packages in requirements.txt
+* Run python notification.py
+* The code is in Python 2.7
+
+#License
+
+GNU General Public License v3 (GPLv3)
+
+#Bug Report
+
+Issue it here: https://github.com/vozille/CricScore/issues
+
---
+++ notification.py
@@ -0,0 +1,46 @@
+"""Cricket Score.
+
+Usage: 
+    notification.py -time T
+    notification.py -h --help
+
+T : time between notifications in seconds
+  
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+
+"""
+
+from gi.repository import Notify
+import json
+import cricbuzz
+import time
+from docopt import docopt
+
+
+while True:
+    arguments = docopt(__doc__, version='1.0')
+    cricbuzz.get_cricket_scores()
+
+    with open('data.json') as data_file:
+        cricket_matches = json.load(data_file)
+
+    message = '\n'
+    for series in cricket_matches:
+        message += cricket_matches[series]["Batting team"] + ': '
+        message += cricket_matches[series]["Batting Team Runs"] + '/'
+        message += cricket_matches[series]["Batting Team Wickets"] + ' (Overs : '
+        message += cricket_matches[series]["Batting Team Overs"] + ')\n'
+        try:
+            message += cricket_matches[series]["Bowling team"] + ': '
+            message += cricket_matches[series]["Bowling Team Runs"] + '/'
+            message += cricket_matches[series]["Bowling Team Wickets"] + ' (Overs : '
+            message += cricket_matches[series]["Bowling Team Overs"] + ')\n'
+        except TypeError:
+            message += "Did not bat"
+        message += '-------------------------------------------------\n'
+    notifier = Notify.init("alert")
+    notifier = Notify.Notification.new(message)
+    notifier.show()
+    time.sleep(int(arguments['T']))
